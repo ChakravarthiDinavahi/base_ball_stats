f718e58084208034416f7f41c01c184f93671f5e
Chakravarthi Dinavahi <cdinavahi@ecisolutions.com>
2021-03-05 22:11:08 +0530

    angular changes

--- app/controllers/base_ball_stats_controller.rb
+++ app/controllers/base_ball_stats_controller.rb
@@ -2,4 +2,9 @@ class BaseBallStatsController < ApplicationController
   def index
     @pagy, @base_ball_stats = pagy(BaseBallStat.all)
   end
+
+  def logs
+    @pagy, @base_ball_stats = pagy(BaseBallStat.all)
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/base_ball_stats", to: "base_ball_stats#index"
+  get "/", to: "base_ball_stats#index"
+  get "/gets", to: "base_ball_stats#gets"
+
 
 end
