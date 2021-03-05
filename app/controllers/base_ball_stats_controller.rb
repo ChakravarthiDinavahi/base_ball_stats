387752b7cb01678a4c8d41fdc65a3f4260cec458
Chakravarthi Dinavahi <cdinavahi@ecisolutions.com>
2021-03-05 21:47:31 +0530

    Infinite scroll changes

--- app/controllers/base_ball_stats_controller.rb
+++ app/controllers/base_ball_stats_controller.rb
@@ -1,5 +1,10 @@
 class BaseBallStatsController < ApplicationController
   def index
-    @pagy, @base_ball_stats = pagy(BaseBallStat.all)
+    # @pagy, @base_ball_stats = pagy(BaseBallStat.all)
+    # offset = params[:offset].presence
+    orderby= params[:orderby].present? ? params[:orderby]: 'avg' 
+    # @base_ball_stats = BaseBallStat.all.order("#{orderby} DESC").page(params[:page])
+
+    # @base_ball_stats.offset(offset).limit(30).order("created_at DESC")
   end
 end
